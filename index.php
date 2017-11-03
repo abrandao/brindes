eba0b0555585884d7beae820516b3ef0e6961ee9
abrandao <andersonbrandaolustosa@gmail.com>
2017-11-03 20:33:18 -0200

    index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+ 
+    <meta name="robots" content="noindex, nofollow, nosnippet, noodp, noarchive, noimageindex" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="Brindes" />
+    <meta name="author" content="Anderson Brandão <brandao@weblogos.com.br" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <menu http-equiv="pragma" content="no-cache" />    
+    
+    <title>Brindes</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Javascript JS -->
+    <script src="bootstrap.min.js" type="javascript"></script>    
+  </head>
+  <body>
+    <h1>Brindes</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   
+   
+  </body>
+</html>
