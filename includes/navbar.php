28f5e976a7a3fd5fbaabacbd9e34192fdb06d3ca
abrandao <andersonbrandaolustosa@gmail.com>
2017-11-07 19:28:50 -0200

    includes and navbar

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+  <a href="#" class="navbar-brand">Brindes</a>
+  <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>           -->
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item"><a class="nav-link" href="#">Início</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Quem Somos</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Produtos</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+  </ul>
+  </div>
+</nav>
