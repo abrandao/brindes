e7f2c9b57d1e136ae3d48219701134f6baa6a91c
abrandao <andersonbrandaolustosa@gmail.com>
2017-11-07 09:18:57 -0200

    images upload

---
+++ admin/products/register.php
@@ -0,0 +1,4 @@
+<form method="POST" action="upload.php" enctype="multipart/form-data">
+  <input type="file" name="fileUpload">
+  <button type="submit">Send</button>
+</form>
---
+++ admin/products/upload.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $file = $_FILES["fileUpload"];
+  if ($file["error"]) {
+    throw new Exception("Error: " . $file["error"]);    
+  }
+  $dirUploads = "uploads";
+  if (!is_dir($dirUploads)) {
+    mkdir($dirUploads);
+  }
+  if (move_uploaded_file($file["tmp_name"], $dirUploads . DIRECTORY_SEPARATOR . $file["name"])) {
+    echo "Upload realizado com sucesso!";    
+  } else {
+    throw new Exception("Não foi possível realizar o upload.");
+  }
+}
