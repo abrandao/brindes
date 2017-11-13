bb5af5a40516a4f376403699e806d55ca01abc03
abrandao <andersonbrandaolustosa@gmail.com>
2017-11-13 08:51:06 -0200

    PDO

--- admin/class/Sql.php
+++ admin/class/Sql.php
@@ -3,5 +3,27 @@
     private $conn;
     public function __construct(){
          $this->conn = new PDO("mysql:host=localhost;dbname=brindes","brandao","sistema");
-    }            
+    }
+
+    private function setParams($statement, $parameters = array()) {
+      foreach ($parameters as $key => $value) {
+        $this->setParams($statement, $key, $value);
+      }
+    }
+
+    private function setParams($statement, $key, $value) {
+      $statement->bindParam($key, $value);
+    }
+
+    public function query($rawQuery, $params = array()) {
+      $stmt = $this->conn->prepare($rawQuery);
+      $this->setParams($stmt, $params);
+      $stmt->execute();
+      return $stmt;
+    }
+
+    public function select($rawQuery, $params = array()):array {
+      $stmt = $this->query($rawQuery, $params);
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
   }
