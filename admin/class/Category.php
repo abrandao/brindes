4748e8a25fdb6d5afa4fb564ed3b81ffca5c8263
abrandao <andersonbrandaolustosa@gmail.com>
2017-12-04 18:03:18 -0200

    retrieving data

--- admin/class/Category.php
+++ admin/class/Category.php
@@ -18,9 +18,31 @@ class Category {
   public function setCategory($value) {
     $this->category = $value;
   }
+
+  public function setData($data) {
+    $this->setId($data['id']);
+    $this->setCategory($data['category']);   
+  }
   
   public static function getList() {
     $sql = new Sql();
     return $sql->select("SELECT * FROM categories ORDER BY id;");
+  }
+
+  public function loadById($id) {
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM categories  WHERE id = :ID", array(
+      ":ID"=>$id
+    ));    
+    if (count($results) > 0) {
+      $this->setData($results[0]);
+    }
+  }
+
+  public function __toString(){
+		return json_encode(array(
+			"id"=>$this->getId(),
+      "category"=>$this->getCategory()      		
+		));
   }
 }
--- admin/testcrud.php
+++ admin/testcrud.php
@@ -2,10 +2,10 @@
 require_once("config.php");
 
 //$product = new Product();
-//$product->loadById(13);
+//$product->loadById(10);
 //$product->deleteImages(13);
 //$product->delete();
-
+//echo $product;
 
 //Listando
 //$product = Product::getList();
@@ -14,5 +14,9 @@ require_once("config.php");
 //$category = new Category();
 //$category->getList();
 
-$category = Category::getList();
-echo json_encode($category);
+//$category = Category::getList();
+//echo json_encode($category);
+
+$category = new Category();
+$category->loadById(2);
+echo $category;
