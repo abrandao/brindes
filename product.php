fbcfbe46f01e1c655dff04c47a5f6eea78061c81
abrandao <andersonbrandaolustosa@gmail.com>
2017-12-18 17:35:11 -0200

    product page v.01

--- product.php
+++ product.php
@@ -10,23 +10,17 @@ require_once("admin/class/Sql.php");
 	<div class="txt-heading">Produtos</div>
 	<?php
 	$db_handle = new Sql();
-	$product_array = $db_handle->runQuery("SELECT * FROM products ORDER BY id ASC");
-	if (!empty($product_array)) { 
-		foreach($product_array as $key=>$value){
+	$product_array = $db_handle->runQuery("SELECT * FROM products WHERE id = 9");	
 	?>
 		<div class="product-item">
 			<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
-			<div class="product-image"><a href="product.php?action=code="><img src="<?php echo "admin/products/" . $product_array[$key]["upfile"] . "/" . $product_array[$key]["upfile"] . "_0.jpg"; ?>"></a></div>
+			<div class="product-image"><a href="shopcart.php"><img src="<?php echo "admin/products/" . $product_array[$key]["upfile"] . "/" . $product_array[$key]["upfile"] . "_0.jpg"; ?>"></a></div>
 			<br>
 			<div><a href="product.php"><?php echo $product_array[$key]["title"]; ?></a></div>
 			<div class="product-price"><?php echo "Quantidade " . $product_array[$key]["qtd_min"]; ?></div>
 			<div><input type="text" name="quantity" value="1" size="2" /><input type="submit" value="Adicionar" class="btnAddAction" /></div>
 			</form>
 		</div>
-	<?php
-			}
-	}
-	?>
 </div>
 </body>
 </html>
