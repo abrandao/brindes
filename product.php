836bc0ed291a4d98f6e8bab1ca0183f671b18a8b
abrandao <andersonbrandaolustosa@gmail.com>
2017-12-28 11:58:30 -0200

    product page with fields

--- product.php
+++ product.php
@@ -22,6 +22,17 @@ $product_array = $db_handle->runQuery("SELECT * FROM products WHERE code = '$cdd
 			<br>
 			<div><a href="product.php"><?php echo $product_array[$key]["title"]; ?></a></div>
 			<div class="product-price"><?php echo "Quantidade " . $product_array[$key]["qtd_min"]; ?></div>
+			<div><p>Código: <?php echo $product_array[$key]["code"]; ?></p></div>
+			<div><p>Tag: <?php echo $product_array[$key]["tag"]; ?></p></div>
+			<div><p>Categoria: <?php echo $product_array[$key]["category"]; ?></p></div>			
+			<div><p>Quantidade 1: <?php echo $product_array[$key]["qtd1"]; ?></p></div>
+			<div><p>Quantidade 2: <?php echo $product_array[$key]["qtd2"]; ?></p></div>
+			<div><p>Quantidade 3: <?php echo $product_array[$key]["qtd3"]; ?></p></div>
+			<div><p>Tamanho: <?php echo $product_array[$key]["size"]; ?></p></div>
+			<div><p>Gravação:  <?php echo $product_array[$key]["printing"]; ?></p></div>
+			<div><p>Tipo de imporessão: <?php echo $product_array[$key]["print_type"]; ?></p></div>
+			<div>Descrição: <?php echo $product_array[$key]["description"]; ?></p></div>
+			<div><p>Comentário: <?php echo $product_array[$key]["comments"]; ?></p></div>
 			<div><input type="text" name="quantity" value="1" size="2" /><input type="submit" value="Adicionar" class="btnAddAction" /></div>
 			</form>
 		</div>
