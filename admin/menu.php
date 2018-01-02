6806f5c5764163ba31132665c43d3b6104a8d323
abrandao <andersonbrandaolustosa@gmail.com>
2018-01-02 09:40:50 -0200

    menu new links

--- admin/menu.php
+++ admin/menu.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 <br>
 <ul class="navbar-nav mr-auto">
     <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
-    <li class="nav-item"><a class="nav-link" href="admin/products/upload.php">Registrar Produtos</a></li>
-    <li class="nav-item"><a class="nav-link" href="admin/categories/register.php">Registrar categoria</a></li>
-    <li class="nav-item"><a class="nav-link" href="admin/categories/registered.php">Categorias registradas</a></li>
+    <li class="nav-item"><a class="nav-link" href="products/upload.php">Registrar Produtos</a></li>
+    <li class="nav-item"><a class="nav-link" href="categories/register.php">Registrar categoria</a></li>
+    <li class="nav-item"><a class="nav-link" href="categories/registered.php">Categorias registradas</a></li>
   </ul>
