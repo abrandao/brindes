47cc1f241a3898acd7161b159a7d797d89eb6738
abrandao <andersonbrandaolustosa@gmail.com>
2018-01-03 11:18:13 -0200

    listing categories

---
+++ admin/cruds/categories/list.php
@@ -0,0 +1,25 @@
+<?php
+// Initialize the session
+session_start();
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: ../../login/login.php");
+  exit;
+}
+?>
+<h3>Lista de categorias registradas:</h3>
+<?php
+
+require_once("../../class/Sql.php");
+
+//Retrieving content
+  $db_handle = new Sql();
+	$category_array = $db_handle->runQuery("SELECT * FROM categories ORDER BY id ASC");
+	if (!empty($category_array)) { 
+		foreach($category_array as $key=>$value){			
+	?>
+      <div><strong><?php echo $category_array[$key]["id"]; ?></strong>
+			<strong><?php echo $category_array[$key]["category"]; ?></strong></div>
+<?php	
+			}
+	}
--- admin/menu.php
+++ admin/menu.php
@@ -15,5 +15,5 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
     <li class="nav-item"><a class="nav-link" href="products/upload.php">Registrar Produtos</a></li>
     <li class="nav-item"><a class="nav-link" href="cruds/categories/register.php">Registrar categoria</a></li>
-    <li class="nav-item"><a class="nav-link" href="cruds/categories/registered.php">Categorias registradas</a></li>
+    <li class="nav-item"><a class="nav-link" href="cruds/categories/list.php">Categorias registradas</a></li>
   </ul>
