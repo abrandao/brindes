e19ab224151aa8b7181bc84ab481a44b8ebc7f38
abrandao <andersonbrandaolustosa@gmail.com>
2018-01-03 10:20:05 -0200

    navbar links improvements and new pages

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,14 +1,12 @@
-<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
-  <a href="index.php" class="navbar-brand">Brindes</a>
-  <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>           -->
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">  
   <div class="collapse navbar-collapse" id="navbarCollapse">
   <ul class="navbar-nav mr-auto">
-    <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Quem Somos</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Produtos</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php">Conheça a Epontual</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Clientes</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Lançamentos</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Promoções</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Tipos de Gravação</a></li>
+    <li class="nav-item"><a class="nav-link" href="#">Fale Conosco</a></li>
   </ul>
   </div>
 </nav>
