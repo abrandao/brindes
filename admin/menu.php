a9c09f11eeee1ced028a903e48e2626703dac467
abrandao <andersonbrandaolustosa@gmail.com>
2018-01-03 11:28:46 -0200

    new menu links

--- admin/menu.php
+++ admin/menu.php
@@ -12,7 +12,7 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 
 <br>
 <ul class="navbar-nav mr-auto">
-    <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
+    <li class="nav-item"><a class="nav-link" href="../index.php">Início</a></li>
     <li class="nav-item"><a class="nav-link" href="products/upload.php">Registrar Produtos</a></li>
     <li class="nav-item"><a class="nav-link" href="cruds/categories/register.php">Registrar categoria</a></li>
     <li class="nav-item"><a class="nav-link" href="cruds/categories/list.php">Categorias registradas</a></li>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,12 +1,13 @@
 <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">  
   <div class="collapse navbar-collapse" id="navbarCollapse">
   <ul class="navbar-nav mr-auto">
-    <li class="nav-item"><a class="nav-link" href="index.php">Conheça a Epontual</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Clientes</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Lançamentos</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Promoções</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Tipos de Gravação</a></li>
-    <li class="nav-item"><a class="nav-link" href="#">Fale Conosco</a></li>
+    <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
+    <li class="nav-item"><a class="nav-link" href="pages/know.php">Conheça a Epontual</a></li>
+    <li class="nav-item"><a class="nav-link" href="pages/clients.php">Clientes</a></li>
+    <li class="nav-item"><a class="nav-link" href="pages/releases.php">Lançamentos</a></li>
+    <li class="nav-item"><a class="nav-link" href="pages/promotions.php">Promoções</a></li>
+    <li class="nav-item"><a class="nav-link" href="pages/printing.php">Tipos de Gravação</a></li>
+    <li class="nav-item"><a class="nav-link" href="pages/contact.php">Fale Conosco</a></li>
   </ul>
   </div>
 </nav>
