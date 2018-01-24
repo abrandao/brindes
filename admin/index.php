7523b8c3323b14b9f13a20eae40e5a08cbee5b96
abrandao <andersonbrandaolustosa@gmail.com>
2018-01-24 10:52:06 -0200

    renaming admin/menu.php to admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -7,3 +7,14 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
   header("location: ../admin/login/login.php");
   exit;
 }
+?>
+
+<h3>Menu Provisório</h3>
+
+<br>
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item"><a class="nav-link" href="../index.php">Início</a></li>
+    <li class="nav-item"><a class="nav-link" href="products/upload.php">Registrar Produtos</a></li>
+    <li class="nav-item"><a class="nav-link" href="cruds/categories/register.php">Registrar categoria</a></li>
+    <li class="nav-item"><a class="nav-link" href="cruds/categories/list.php">Categorias registradas</a></li>
+  </ul>
