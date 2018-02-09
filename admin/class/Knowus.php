a8aaae572c3a1040fc9fd60de63d68f5af1a0947
abrandao <andersonbrandaolustosa@gmail.com>
2018-02-09 10:34:44 -0200

    new knowus link in navbars

---
+++ admin/class/Knowus.php
@@ -0,0 +1,29 @@
+<?php
+class Knowus {
+  private $id;
+  private $title;
+  private $article;
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function setId($value) {
+    $this->id = $value;
+  }
+
+  public function getTitle() {
+    return $this->title;
+  }
+
+  public function setTitle($value) {
+    $this->title = $value;
+  }
+
+  public function getArticle() {
+    return $this->id;
+  }
+
+  public function setArticle($value) {
+    $this->article = $value;
+  }
--- pages/request.php
+++ pages/request.php
@@ -90,7 +90,7 @@ echo "<br>";
 <nav class="navbar navbar-expand navbar-light bg-light fixed-top" id="navbarCollapse">
   <ul class="navbar-nav">
     <li class="nav-item"><a class="nav-link" href="../index.php">Início</a></li>
-    <li class="nav-item"><a class="nav-link" href="../pages/know.php">Conheça a Epontual</a></li>
+    <li class="nav-item"><a class="nav-link" href="../pages/knowus.php">Conheça a Epontual</a></li>
     <li class="nav-item"><a class="nav-link"  href="../pages/clients.php">Clientes</a></li>
       <li class="nav-item"><a class="nav-link"  href="../pages/releases.php">Lançamentos</a></li>
       <li class="nav-item"><a class="nav-link"  href="../pages/promotions.php">Promoções</a></li>
