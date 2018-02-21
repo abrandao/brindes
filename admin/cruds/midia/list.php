41cbf65552b3de89ce3a9667ed6fa1c4175ffd65
abrandao <andersonbrandaolustosa@gmail.com>
2018-02-21 15:54:50 -0300

    midia/list.php layout improvement

--- admin/cruds/midia/list.php
+++ admin/cruds/midia/list.php
@@ -1,5 +1,5 @@
 <?php
-
+  
   // Initialize the session
   session_start();
   // If session variable is not set it will redirect to login page
@@ -7,6 +7,37 @@
     header("location: ../login/login.php");
     exit;
   }
+?>
+
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+ 
+    <meta name="robots" content="noindex, nofollow, nosnippet, noodp, noarchive, noimageindex" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="Brindes" />
+    <meta name="author" content="Anderson Brandão" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <menu http-equiv="pragma" content="no-cache" />    
+    
+    <title>Brindes</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../../css/bootstrap.css">   
+    <link rel="stylesheet" href="../../../css/style.css">     
+  </head>
+
+  <div class="container">
+     <div class="row">
+
+<?php
 
     $images = scandir("../../../includes/img/midia/");
 
@@ -14,11 +45,19 @@
     
     foreach($images as $key=>$img){	
       if(!in_array($img, array(".", ".."))) {
-      echo $images[$key];
-      echo "<br>";
-
-?>
-      <img src="<?php echo "../../../includes/img/midia/" . $images[$key]; ?>">
-<?php
+?>    
+    <div class="col-lg-4">
+      <img class="img-thumbnail rounded" style="width: 200px; height: 200px;" src="<?php echo "../../../includes/img/midia/" . $images[$key];?>"></img>
+      <br>
+      <?php echo $images[$key] ?>
+      <br>      
+      <textarea cols="50"><?php echo "../../../includes/img/midia/" . $images[$key]; ?></textarea>
+      <hr>
+    </div>
+    <br>
+<?php    
       }
-		}     
+    }
+?>
+    </div>
+  </div>     
