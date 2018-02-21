f7f7a6a79db914b87955c769398d2b3e29180702
abrandao <andersonbrandaolustosa@gmail.com>
2018-02-21 18:49:54 -0300

    some adjustments

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,9 +9,9 @@
     <ul class="navbar-nav">
       <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
       <li class="nav-item"><a class="nav-link" href="pages/knowus.php">Conheça a Epontual</a></li>    
-      <li class="nav-item"><a class="nav-link" ef="pages/releases.php">Lançamentos</a></li>
-      <li class="nav-item"><a class="nav-link" ef="pages/promotions.php">Promoções</a></li>    
-      <li class="nav-item"><a class="nav-link" href="pages/contact.php">Fale nosco</a></li>
+      <li class="nav-item"><a class="nav-link" href="pages/releases.php">Lançamentos</a></li>
+      <li class="nav-item"><a class="nav-link" href="pages/promotions.php">Promoções</a></li>    
+      <li class="nav-item"><a class="nav-link" href="pages/contact.php">Fale Conosco</a></li>
     </ul>
 
     <form method="post" action="pages/search.php" class="form-inline">
