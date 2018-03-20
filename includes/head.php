82aa5a128e1601ca66028b2879b04830a97f6f24
abrandao <andersonbrandaolustosa@gmail.com>
2018-03-20 15:58:45 -0300

    Favicon working on several browsers

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,25 @@
 <!doctype html>
 <html lang="pt-BR">
   <head>
+
+    <!-- Favicon area -->
+    <link rel="apple-touch-icon" sizes="57x57" href="includes/img/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="includes/img/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="includes/img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="includes/img/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="includes/img/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="includes/img/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="includes/img/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="includes/img/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="includes/img/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="includes/img/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="includes/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="includes/img/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="includes/img/favicon/favicon-16x16.png">    
+    <link rel="manifest" href="includes/img/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="includes/img/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
  
     <meta name="robots" content="noindex, nofollow, nosnippet, noodp, noarchive, noimageindex" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -37,20 +56,7 @@
     <!-- Javascript JS -->
     <script src="../js/bootstrap.js" type="javascript"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
-    <!-- Favicon area -->
-    <link rel="apple-touch-icon" sizes="180x180" href="includes/img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="includes/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="includes/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="includes/img/favicon/site.webmanifest">
-    <link rel="mask-icon" href="includes/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="includes/img/favicon/favicon.ico">
-    <meta name="apple-mobile-web-app-title" content="Epontual Brindes">
-    <meta name="application-name" content="Epontual Brindes">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="includes/img/favicon/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>    
   
   <?php
   
