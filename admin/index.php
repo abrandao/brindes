dafea1388e87ee096088ca9471667fca4067e0a4
abrandao <andersonbrandaolustosa@gmail.com>
2018-03-27 19:42:00 -0300

    New links in admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -22,7 +22,9 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     <hr>
     <li class="nav-item"><a class="nav-link" href="cruds/knowus/knowus.php"><strong>REGISTRAR "Conheça a Epontual"</strong></a></li>
     <hr>
-    <li class="nav-item"><a class="nav-link" href="cruds/slider/update.php"><strong>MUDAR IMAGENS DO SLIDE</strong></a></li>
+    <li class="nav-item"><a class="nav-link" href="cruds/slider/list.php"><strong>LISTAR SLIDES</strong></a></li>
+    <li class="nav-item"><a class="nav-link" href="cruds/slider/update.php"><strong>UPDATE DE SLIDES</strong></a></li>
+    <li class="nav-item"><a class="nav-link" href="cruds/slider/upload.php"><strong>CADASTRAR SLIDES</strong></a></li>
     <hr>
     <li class="nav-item"><a class="nav-link" href="cruds/midia/list.php"><strong>LISTAR ARQUIVOS DE MÍDIA</strong></a></li>
     <li class="nav-item"><a class="nav-link" href="cruds/midia/upload.php"><strong>FAZER UPLOAD DE ARQUIVOS DE MÍDIA</strong></a></li>
